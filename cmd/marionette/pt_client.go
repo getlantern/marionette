a75c18ecce62a7ad63c7fe5cdb0a3213c0b9d5a9
Ben Johnson <benbjohnson@yahoo.com>
2018-03-13 14:22:50 -0600

    Clean up pluggable transport code.

--- cmd/marionette/pt_client.go
+++ cmd/marionette/pt_client.go
@@ -12,111 +12,22 @@ import (
 	"sync"
 	"syscall"
 
+	pt "git.torproject.org/pluggable-transports/goptlib.git"
 	"github.com/redjack/marionette"
 	"github.com/redjack/marionette/mar"
-
 	"go.uber.org/zap"
-
-	pt "git.torproject.org/pluggable-transports/goptlib.git"
 )
 
-var handlerChan = make(chan int)
-
-func copyLoop(stream net.Conn, or net.Conn) {
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	go func() {
-		io.Copy(or, stream)
-		wg.Done()
-	}()
-
-	go func() {
-		io.Copy(stream, or)
-		wg.Done()
-	}()
-
-	wg.Wait()
+type PTClientCommand struct {
+	wg sync.WaitGroup
 }
-
-func handleClient(connection *pt.SocksConn, doc *mar.Document) {
-
-	host, _, err := net.SplitHostPort(connection.Req.Target)
-
-	if err != nil {
-		connection.Reject()
-		return
-	}
-
-	handlerChan <- 1
-
-	defer func() {
-		handlerChan <- -1
-		connection.Close()
-		log.Printf("Disconnected from Marionette host: %s", host)
-	}()
-
-	streamSet := marionette.NewStreamSet()
-	defer streamSet.Close()
-
-	log.Printf("Connecting to Marionette server: %s", host)
-
-	// Create dialer to remote server.
-	dialer, err := marionette.NewDialer(doc, host, streamSet)
-	if err != nil {
-		log.Printf("Unable to create dialer: %s", err)
-		connection.Reject()
-		return
-	}
-	defer dialer.Close()
-	log.Printf("Connected!")
-
-	stream, err := dialer.Dial()
-	if err != nil {
-		log.Printf("Unable to connect to server: %s", err)
-		connection.Reject()
-		return
-	}
-	defer stream.Close()
-
-	err = connection.Grant(nil)
-
-	if err != nil {
-		return
-	}
-
-	log.Printf("Entering copyLoop")
-	copyLoop(stream, connection)
-}
-
-func acceptLoop(listener *pt.SocksListener, doc *mar.Document) {
-	defer listener.Close()
-
-	for {
-		connection, err := listener.AcceptSocks()
-
-		if err != nil {
-			netErr, ok := err.(net.Error)
-
-			if ok && netErr.Temporary() {
-				continue
-			}
-
-			return
-		}
-
-		go handleClient(connection, doc)
-	}
-}
-
-type PTClientCommand struct{}
 
 func NewPTClientCommand() *PTClientCommand {
 	return &PTClientCommand{}
 }
 
 func (cmd *PTClientCommand) Run(args []string) error {
-	fs := flag.NewFlagSet("marionette-ptclient", flag.ContinueOnError)
+	fs := flag.NewFlagSet("marionette-pt-client", flag.ContinueOnError)
 	var (
 		format  = fs.String("format", "", "Format name and version")
 		logFile = fs.String("log-file", "", "Path to log file.")
@@ -127,7 +38,6 @@ func (cmd *PTClientCommand) Run(args []string) error {
 
 	if *logFile != "" {
 		file, err := os.OpenFile(*logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
-
 		if err != nil {
 			fmt.Errorf("Unable to open log file: %s", err)
 		}
@@ -154,71 +64,116 @@ func (cmd *PTClientCommand) Run(args []string) error {
 		return err
 	}
 
-	// We always use the production logger when running
-	// as a PT.
+	// We always use the production logger when running as a PT.
 	logger, err := zap.NewProduction()
 	if err != nil {
-		return nil
+		return err
 	}
 	marionette.Logger = logger
 
 	clientInfo, err := pt.ClientSetup(nil)
-
 	if err != nil {
 		return err
-	}
-
-	if clientInfo.ProxyURL != nil {
-		// FIXME: Ugh? I wonder if this can actually
-		// happen.
-		return nil
+	} else if clientInfo.ProxyURL != nil {
+		return errors.New("marionette: PT proxy url not provided")
 	}
 
 	listeners := make([]net.Listener, 0)
-
 	for _, methodName := range clientInfo.MethodNames {
-		if methodName == "marionette" {
-			listener, err := pt.ListenSocks("tcp", "127.0.0.1:0")
-
-			if err != nil {
-				pt.CmethodError(methodName, err.Error())
-				break
-			}
-
-			go acceptLoop(listener, doc)
-
-			pt.Cmethod(methodName, listener.Version(), listener.Addr())
-			listeners = append(listeners, listener)
-		} else {
+		if methodName != "marionette" {
 			pt.CmethodError(methodName, "no such method")
 		}
+
+		listener, err := pt.ListenSocks("tcp", "127.0.0.1:0")
+		if err != nil {
+			pt.CmethodError(methodName, err.Error())
+			break
+		}
+
+		cmd.wg.Add(1)
+		go func() { defer cmd.wg.Done(); cmd.acceptLoop(listener, doc) }()
+
+		pt.Cmethod(methodName, listener.Version(), listener.Addr())
+		listeners = append(listeners, listener)
 	}
 	pt.CmethodsDone()
 
-	numHandlers := 0
-	var sig os.Signal
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGTERM)
-
-	sig = nil
-	for sig == nil {
-		select {
-		case n := <-handlerChan:
-			numHandlers += n
-		case sig = <-sigChan:
-		}
-	}
+	<-sigChan
 
 	for _, listener := range listeners {
 		listener.Close()
 	}
-
-	for n := range handlerChan {
-		numHandlers += n
-		if numHandlers == 0 {
-			break
-		}
-	}
+	cmd.wg.Wait()
 
 	return nil
+}
+
+func (cmd *PTClientCommand) acceptLoop(listener *pt.SocksListener, doc *mar.Document) {
+	defer listener.Close()
+
+	for {
+		connection, err := listener.AcceptSocks()
+		if err != nil {
+			if err, ok := err.(net.Error); ok && err.Temporary() {
+				continue
+			}
+			return
+		}
+
+		cmd.wg.Add(1)
+		go func() { defer cmd.wg.Done(); cmd.handleConn(connection, doc) }()
+	}
+}
+
+func (cmd *PTClientCommand) handleConn(connection *pt.SocksConn, doc *mar.Document) {
+	host, _, err := net.SplitHostPort(connection.Req.Target)
+	if err != nil {
+		log.Printf("Invalid connection request target: %s", connection.Req.Target)
+		connection.Reject()
+		return
+	}
+
+	log.Printf("Connecting to Marionette server: %s", host)
+	defer connection.Close()
+	defer log.Printf("Disconnected from Marionette host: %s", host)
+
+	streamSet := marionette.NewStreamSet()
+	defer streamSet.Close()
+
+	// Create dialer to remote server.
+	dialer, err := marionette.NewDialer(doc, host, streamSet)
+	if err != nil {
+		log.Printf("Unable to create dialer: %s", err)
+		connection.Reject()
+		return
+	}
+	defer dialer.Close()
+	log.Printf("Connected!")
+
+	// Create a stream through the dialer.
+	stream, err := dialer.Dial()
+	if err != nil {
+		log.Printf("Unable to connect to server: %s", err)
+		connection.Reject()
+		return
+	}
+	defer stream.Close()
+
+	// Allow connection.
+	if err := connection.Grant(nil); err != nil {
+		return
+	}
+
+	log.Printf("Proxying stream to connection")
+	proxyConns(stream, connection)
+}
+
+func proxyConns(a, b net.Conn) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() { defer wg.Done(); io.Copy(a, b) }()
+	go func() { defer wg.Done(); io.Copy(b, a) }()
+	wg.Wait()
 }
--- cmd/marionette/pt_server.go
+++ cmd/marionette/pt_server.go
@@ -8,61 +8,18 @@ import (
 	"net"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 
+	pt "git.torproject.org/pluggable-transports/goptlib.git"
 	"github.com/redjack/marionette"
 	"github.com/redjack/marionette/mar"
-
 	"go.uber.org/zap"
-
-	pt "git.torproject.org/pluggable-transports/goptlib.git"
 )
 
-func handleServerConn(connection net.Conn, serverInfo *pt.ServerInfo) {
-	log.Printf("Client connected: %s", connection.RemoteAddr())
-
-	handlerChan <- 1
-	defer func() {
-		handlerChan <- -1
-		log.Printf("Client disconnected: %s", connection.RemoteAddr())
-	}()
-
-	log.Printf("Connecting to Onion Router")
-	or, err := pt.DialOr(serverInfo, "127.0.0.1:1234", "marionette")
-
-	if err != nil {
-		log.Printf("Unable to connect to Onion Router: %s", err)
-		return
-	}
-
-	defer or.Close()
-
-	log.Printf("Entering copy loop")
-	copyLoop(connection, or)
+type PTServerCommand struct {
+	wg sync.WaitGroup
 }
-
-func acceptServerLoop(listener net.Listener, serverInfo *pt.ServerInfo) {
-	defer listener.Close()
-
-	for {
-                log.Printf("Entering listener.Accept")
-		connection, err := listener.Accept()
-
-                log.Printf("Accepted listener")
-		if err != nil {
-			if e, ok := err.(net.Error); ok && e.Temporary() {
-				continue
-			}
-
-			log.Printf("Error accepting client: %s", err)
-			return
-		}
-
-		go handleServerConn(connection, serverInfo)
-	}
-}
-
-type PTServerCommand struct{}
 
 func NewPTServerCommand() *PTServerCommand {
 	return &PTServerCommand{}
@@ -107,80 +64,106 @@ func (cmd *PTServerCommand) Run(args []string) error {
 		return err
 	}
 
-	// We always use the production logger when running
-	// as a PT.
+	// We always use the production logger when running as a PT.
 	logger, err := zap.NewProduction()
 	if err != nil {
-		return nil
+		return err
 	}
 	marionette.Logger = logger
 
 	// Setup the PT.
 	serverInfo, err := pt.ServerSetup(nil)
-
 	if err != nil {
 		return err
 	}
 
 	listeners := make([]net.Listener, 0)
-
 	for _, bindAddr := range serverInfo.Bindaddrs {
-		if bindAddr.MethodName == "marionette" {
-			log.Printf("Starting Marionette PT")
-
-			// Marionette always listen on port 8081,
-			// so we ignore what Tor is telling us
-			// here. This should probably be fixed.
-			host, port, err := net.SplitHostPort(bindAddr.Addr.String())
-
-			if err != nil {
-				log.Printf("Unable to split host/port: %s", err)
-				pt.SmethodError(bindAddr.MethodName, err.Error())
-				break
-			}
-
-			if port != "8081" {
-				log.Printf("Port wasn't 8081")
-				pt.SmethodError(bindAddr.MethodName, err.Error())
-				break
-			}
-
-			// Start the listener.
-			listener, err := marionette.Listen(doc, host)
-
-			if err != nil {
-				log.Printf("Unable to create listener: %s", err)
-				pt.SmethodError(bindAddr.MethodName, err.Error())
-				break
-			}
-
-			go acceptServerLoop(listener, &serverInfo)
-
-			pt.Smethod(bindAddr.MethodName, listener.Addr())
-			listeners = append(listeners, listener)
-		} else {
+		if bindAddr.MethodName != "marionette" {
 			pt.SmethodError(bindAddr.MethodName, "no such method")
 		}
+
+		log.Printf("Starting Marionette PT")
+
+		// Marionette always listen on port 8081 so we ignore TOR.
+		// This should probably be fixed.
+		host, port, err := net.SplitHostPort(bindAddr.Addr.String())
+		if err != nil {
+			log.Printf("Unable to split host/port: %s", err)
+			pt.SmethodError(bindAddr.MethodName, err.Error())
+			break
+		}
+
+		if port != "8081" {
+			log.Printf("Port wasn't 8081")
+			pt.SmethodError(bindAddr.MethodName, err.Error())
+			break
+		}
+
+		// Start the listener.
+		listener, err := marionette.Listen(doc, host)
+
+		if err != nil {
+			log.Printf("Unable to create listener: %s", err)
+			pt.SmethodError(bindAddr.MethodName, err.Error())
+			break
+		}
+
+		cmd.wg.Add(1)
+		go func() { defer cmd.wg.Done(); cmd.acceptLoop(listener, &serverInfo) }()
+
+		pt.Smethod(bindAddr.MethodName, listener.Addr())
+		listeners = append(listeners, listener)
 	}
 	pt.SmethodsDone()
 
-	numHandlers := 0
-	var sig os.Signal
+	// Wait for SIGTERM.
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGTERM)
+	<-sigChan
 
-	sig = nil
-	for sig == nil {
-		select {
-		case n := <-handlerChan:
-			numHandlers += n
-		case sig = <-sigChan:
-		}
-	}
-
+	// Close listeners and wait for connections to close.
 	for _, listener := range listeners {
 		listener.Close()
 	}
+	cmd.wg.Wait()
 
 	return nil
+}
+
+func (cmd *PTServerCommand) acceptLoop(listener net.Listener, serverInfo *pt.ServerInfo) {
+	defer listener.Close()
+
+	for {
+		log.Printf("Entering listener.Accept")
+		connection, err := listener.Accept()
+
+		log.Printf("Accepted listener")
+		if err != nil {
+			if err, ok := err.(net.Error); ok && err.Temporary() {
+				continue
+			}
+			log.Printf("Error accepting client: %s", err)
+			return
+		}
+
+		cmd.wg.Add(1)
+		go func() { defer cmd.wg.Done(); cmd.handleConn(connection, serverInfo) }()
+	}
+}
+
+func (cmd *PTServerCommand) handleConn(connection net.Conn, serverInfo *pt.ServerInfo) {
+	log.Printf("Client connected: %s", connection.RemoteAddr())
+	defer log.Printf("Client disconnected: %s", connection.RemoteAddr())
+
+	log.Printf("Connecting to Onion Router")
+	or, err := pt.DialOr(serverInfo, "127.0.0.1:1234", "marionette")
+	if err != nil {
+		log.Printf("Unable to connect to Onion Router: %s", err)
+		return
+	}
+	defer or.Close()
+
+	log.Printf("Entering copy loop")
+	proxyConns(connection, or)
 }
